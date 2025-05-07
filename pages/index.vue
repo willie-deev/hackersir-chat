1f3072ed3e79f35d77e5b96d49740d4d66abcd8c
willie-deev <willie.deev@gmail.com>
2025-05-07 23:42:46 +0800

    initialize

---
+++ pages/index.vue
@@ -0,0 +1,172 @@
+<script>
+const page = ref(1)
+</script>
+<template>
+  <div class="next-page">
+    <nuxt-link role="button" @click="goNextPage()">Next Page</nuxt-link>
+  </div>
+  <div class="previous-page">
+    <nuxt-link role="button" @click="goPreviousPage()">Previous Page</nuxt-link>
+  </div>
+  <main class="container">
+    <div class="center">
+      <article id="0" class="page">
+        <div>
+          <h1>網頁開發</h1>
+          <h2>郭兆威</h2>
+        </div>
+      </article>
+
+
+      <article id="1" class="page">
+        <div>
+          <h1>使用框架</h1>
+          <h2>前端</h2>
+          <h3>Nuxt.js (TypeScript)</h3>
+          <h3>Pico CSS</h3>
+          <h2>後端</h2>
+          <h3>Spring Boot (Java)</h3>
+          <h3>MariaDB</h3>
+          <h2>驗證（前後端）</h2>
+          <h3>Logto（oidc）</h3>
+        </div>
+      </article>
+
+      <article id="2" class="page">
+        <div>
+          <h1>伺服器架構</h1>
+          <img src="../assets/workflow.drawio.png" height="622" width="952"/>
+        </div>
+      </article>
+
+      <article id="3" class="page">
+        <div>
+          <h1>Nuxt.js 是啥？</h1>
+          <h3>Server Side Renader 前後端</h3>
+          <h1>為啥使用 Nuxt.js ？</h1>
+          <h3>Nuxt.js 狂熱分子推薦</h3>
+          <h1>PICO css</h1>
+        </div>
+      </article>
+
+      <article id="4" class="page">
+        <div>
+          <h1>驗證流程</h1>
+          <img src="../assets/auth_overflow.drawio.png" height="887" width="872"/>
+        </div>
+      </article>
+
+      <article id="5" class="page">
+        <div>
+          <h1>OIDC 是啥？</h1>
+          <h3>OpenID Connect</h3>
+          <h3>認證通訊協定</h3>
+        </div>
+      </article>
+
+      <article id="6" class="page">
+        <div>
+          <h1>使用者驗證流程</h1>
+          <img src="../assets/user_auth_overflow.png" height="638" width="1104"/>
+        </div>
+      </article>
+
+      <article id="7" class="page">
+        <div>
+          <h1>API Access Token</h1>
+          <img src="../assets/img.png" height="848" width="643"/>
+        </div>
+      </article>
+
+      <article id="8" class="page">
+        <div>
+          <h1>Spring Boot</h1>
+          <h3>Java 應用程式框架</h3>
+          <h3>最少化配置設定</h3>
+        </div>
+      </article>
+
+      <article id="9" class="page">
+        <div>
+          <h1>RESTful API</h1>
+          <h3>統一介面</h3>
+          <h3>無狀態</h3>
+          <h3>分層系統</h3>
+        </div>
+      </article>
+
+      <article id="10" class="page">
+        <div>
+          <h1>WebSocket 是啥 ？</h1>
+          <h3>Socket over HTTP</h3>
+          <h3>用於即時通訊</h3>
+          <h3>允許伺服器主動向客戶端傳資料</h3>
+          <h1>為何使用 Websocket ？</h1>
+          <h3>即時接收新訊息</h3>
+        </div>
+      </article>
+
+      <article id="11" class="page">
+        <div>
+          <h1>成品連結</h1>
+          <a href="https://chat.devvillie.me"><h3 style="color: dodgerblue;">https://chat.devvillie.me</h3></a>
+          <h1>參考資料</h1>
+          <a href="https://www.ibm.com/docs/en/sva/11.0.0?topic=support-oauth-20-oidc-workflows"><h3 style="color: dodgerblue;">https://www.ibm.com/docs/en/sva/11.0.0</h3></a>
+          <br>
+          <a href="https://docs.logto.io"><h3 style="color: dodgerblue;">https://docs.logto.io</h3></a>
+        </div>
+      </article>
+    </div>
+  </main>
+</template>
+
+<style>
+.page {
+  min-height: 100vh;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+.center {
+  text-align: center;
+}
+h1{
+  font-size: 4rem;
+}
+h2{
+  font-size: 3rem;
+}
+h3{
+  font-size: 2.5rem;
+}
+h4{
+  font-size: 2rem;
+}
+.next-page {
+  text-align: right;
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+}
+.previous-page {
+  text-align: left;
+  position: fixed;
+  bottom: 1rem;
+  left: 1rem;
+}
+</style>
+<script setup>
+  import {navigateTo} from "#app";
+  const page = useState('page', () => 0)
+  function goNextPage(){
+    page.value+=1
+    navigateTo('#' + page.value.toString())
+  }
+  function goPreviousPage(){
+    if(page.value <= 0){
+      return
+    }
+    page.value-=1
+    navigateTo('#' + page.value.toString())
+  }
+</script>
